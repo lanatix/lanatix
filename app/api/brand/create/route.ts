62e320d442c12ad8cc0645b36d3ccbe87335e13e
Emmanuel Ngoka <emmanuelngoka778@gmail.com>
2023-09-22 18:20:30 +0100

    bug fix

--- app/api/brand/create/route.ts
+++ app/api/brand/create/route.ts
@@ -10,7 +10,7 @@ export const POST = async (req: NextRequest) => {
   const { walletAddress, ...payload } = await req.json();
   try {
     const alreadyExisting = await prisma.brand.findUnique({
-      where: { walletAddress },
+      where: { walletAddress: walletAddress },
     });
     if (alreadyExisting)
       return NextResponse.json(
